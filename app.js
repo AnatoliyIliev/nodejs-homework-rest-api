02129dcc91b3b943ce5a6c83956f040061badaf6
Anatoliy_Iliev <vekok12@gmail.com>
2021-10-18 17:55:53 +0300

    schema

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const { NAME } = process.env
 const { PASSWORD } = process.env
 
 const DB_HOST = `mongodb+srv://${NAME}:${PASSWORD}@cluster0.ugw2z.mongodb.net/db-contacts?retryWrites=true&w=majority`
+// console.log(DB_HOST)
+const { Schema, model } = mongoose
+
+const productSchema = Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+})
 
 mongoose.connect(DB_HOST, {
   useNewUrlParser: true,
