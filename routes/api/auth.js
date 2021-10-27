762260824c26b61df19376b455f74b9791cac8fa
Anatoliy_Iliev <vekok12@gmail.com>
2021-10-27 17:48:58 +0300

    auth

---
+++ routes/api/auth.js
@@ -0,0 +1,10 @@
+// const express = require('exspress')
+// const { controllerWraper, validation } = require('../../middlewares')
+// const { joiSchema } = require('../../models/user')
+// const { auth: ctrl } = require('../../controllers')
+
+// const router = express.Router()
+
+// router.post('/signup')
+
+// module.exports = router
