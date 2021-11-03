23281e62f6ad711ce3125a0cf4fecff7148fb3cb
Anatoliy_Iliev <vekok12@gmail.com>
2021-11-03 17:50:16 +0200

    start

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs/promises')
+
 const authRouter = require('./routes/api/auth')
 const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
+
+const tempDir = path.join(__dirname, 'temp')
+
+const uploadConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir)
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+  limits: {
+    fileSize: 2048
+  }
+})
+const upload = multer({
+  starage: uploadConfig
+})
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -16,6 +37,12 @@ app.use(express.json())
 
 app.use('/api/auth', authRouter)
 app.use('/api/contacts', contactsRouter)
+
+app.patch('/users/avatars', upload.single('avatar'), async(req, res) => {
+  const { path: tempDir, originalname } = req.file
+  const uploadDir = path.join(__dirname, 'public\\avatars', originalname)
+  await fs.rename(tempDir, uploadDir)
+})
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
